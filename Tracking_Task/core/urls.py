d05244b66d489a26230eeb84ad9fed4db7e80bc1
D6 <konctantun68@gmail.com>
2025-10-24 17:31:05 +0300

    Add Auth. (v.0.1.1)

--- Tracking_Task/core/urls.py
+++ Tracking_Task/core/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from .views import TaskListView, TaskDetailView, TaskCreateView
+from django.contrib.auth import views as auth_views
+from . import views
 
 urlpatterns = [
     path('', TaskListView.as_view(), name='task_list'),
     path('task/<int:pk>/', TaskDetailView.as_view(), name='task_detail'),
     path('task/new/', TaskCreateView.as_view(), name='task_create'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+    path('register/', views.register, name='register'),
 ]
+
--- Tracking_Task/core/views.py
+++ Tracking_Task/core/views.py
@@ -3,6 +3,9 @@ from django.views.generic import ListView, DetailView, CreateView
 from django.urls import reverse_lazy
 from .models import Task
 from django import forms
+from .forms import RegisterForm
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
 
@@ -27,3 +30,17 @@ class TaskCreateView(CreateView):
     form_class = TaskForm
     template_name = 'tasks/task_form.html'
     success_url = reverse_lazy('task_list')
+
+def register(request):
+    form = RegisterForm(request.POST or None)
+    if request.method == 'POST' and form.is_valid():
+        username = form.cleaned_data['username']
+        password = form.cleaned_data['password']
+        if User.objects.filter(username=username).exists():
+            messages.error(request, "Такий користувач уже існує.")
+        else:
+            User.objects.create_user(username=username, password=password)
+            messages.success(request, "")
+            return redirect('login')  # после регистрации — на логин
+    return render(request, 'register.html', {'form': form})
+
